de002163024e7c9ed6c10d44a4a079c085b9aab6
alex <96148536+Aleksei-Kibardin@users.noreply.github.com>
2023-03-06 12:19:31 +0300

    add show, hide function

--- js/index.js
+++ js/index.js
@@ -2,11 +2,26 @@ const btnNext = document.querySelector('.next'),
     btnPrev = document.querySelector('.prev'),
     total = document.querySelector('#total'),
     current = document.querySelector('#current'),
-    constent = document.querySelectorAll('.content')
+    content = document.querySelectorAll('.content')
 
 let index = 1;
 
 current.textContent = index;
+
+function hideContent() {
+    content.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+}
+
+function showContent(i) {
+    content[i].classList.add('show', 'fade');
+    content[i].classList.remove('hide');
+}
+
+hideContent();
+showContent(index - 1);
 
 btnNext.addEventListener('click', () => {
     if (index < 4) {
@@ -16,6 +31,8 @@ btnNext.addEventListener('click', () => {
         index = 1;
         current.textContent = index;
     }
+    hideContent();
+    showContent(index - 1);
 })
 
 btnPrev.addEventListener('click', () => {
@@ -26,4 +43,6 @@ btnPrev.addEventListener('click', () => {
         index = 4;
         current.textContent = index;
     }
+    hideContent();
+    showContent(index - 1);
 })
